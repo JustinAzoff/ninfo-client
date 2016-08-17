62c5bab5dbca17b02ad160d84555fba91b95a514
Justin Azoff <justin.azoff@gmail.com>
2016-08-17 16:23:58 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='ninfo-client',
+    version='0.1.0',
+    zip_safe=True,
+    include_package_data=True,
+    py_modules = ["ninfo_client"],
+    install_requires=[
+        "requests",
+    ],
+    entry_points = {
+        'console_scripts': [
+            'ninfo-client = ninfo_client:main',
+        ]
+    }
+)
